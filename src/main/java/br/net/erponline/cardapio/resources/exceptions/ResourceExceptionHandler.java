87682c0ea447667d38c8333b7bb237efba7a0e19
Flavio F Galanti <flavio@ds-info.com>
2019-12-11 14:58:32 -0300

    Ajustes nos Execptions

--- src/main/java/br/net/erponline/cardapio/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/net/erponline/cardapio/resources/exceptions/ResourceExceptionHandler.java
@@ -7,10 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import br.net.erponline.cardapio.services.exceptions.DatabaseException;
+import br.net.erponline.cardapio.services.exceptions.FileException;
 import br.net.erponline.cardapio.services.exceptions.ObjectNotFoundException;
 import br.net.erponline.cardapio.utils.Translators;
 
@@ -22,7 +25,7 @@ public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
-		String error = resourceBundle.getString("error.NotFound");
+		String error = resourceBundle.getString("error.notfound");
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
@@ -39,5 +42,27 @@ public class ResourceExceptionHandler {
 		
 		return ResponseEntity.status(status).body(err);
 	}
-	
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		String error = resourceBundle.getString("error.datavalidation");
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		
+		ValidationError err = new ValidationError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		
+		for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+			err.addErrors(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(FileException.class)
+	public ResponseEntity<StandardError> file(FileException e, HttpServletRequest request) {
+		String error = resourceBundle.getString("error.file");
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}	
 }
